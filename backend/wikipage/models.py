ff196b8105f5a55a886a46528dd9fca59e0f99f6
Sa Wang <wangsa19960109@gmail.com>
2023-10-21 10:26:48 -0700

    [backend] Accelerate database query operations

--- backend/wikipage/models.py
+++ backend/wikipage/models.py
@@ -11,5 +11,10 @@ class Wikipage(models.Model):
     title = models.CharField(max_length=32)
     views = models.IntegerField(default=0)
 
+    class Meta:
+        indexes = [
+            models.Index(fields=['date', 'title'], name='idx_date_title')
+        ]
+
     def __str__(self):
         return self.title
