fbce9741eae5de7d6dd78179632d7bc5edaeb60a
Sa Wang <wangsa19960109@gmail.com>
2023-10-26 09:43:22 -0700

    [backend] Modify table structure and parse user input

--- backend/wikipage/models.py
+++ backend/wikipage/models.py
@@ -3,17 +3,15 @@ import datetime
 
 # Create your models here.
 
-def get_title():
-    return 'title_test'
-
 class Wikipage(models.Model):
-    date = models.DateField(default=datetime.date(2023, 1, 1))
-    title = models.CharField(max_length=32)
-    views = models.IntegerField(default=0)
+    month = models.CharField(max_length=32, default='0000-00')
+    keyword = models.CharField(max_length=32, default='')
+    title = models.CharField(max_length=128, default='')
+    views = models.CharField(max_length=256)
 
     class Meta:
         indexes = [
-            models.Index(fields=['date', 'title'], name='idx_date_title')
+            models.Index(fields=['month', 'keyword', 'title'], name='idx_MKT')
         ]
 
     def __str__(self):
--- backend/wikipage/views.py
+++ backend/wikipage/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from .serializers import *
 from .models import *
 from wikipage.models import Wikipage
+import json
 
 # Create your views here.
 def hello(response):
@@ -20,13 +21,61 @@ class WikipageView(generics.CreateAPIView):
 
 class GetRequest(APIView):
     def get(self, request):
-        result = Wikipage.objects.filter(date='2023-01-01', title='Sa_Wang')
+        result = Wikipage.objects.filter(month='2023-01', keyword='Sa')
         if result.count() == 0:
             return Response("No such record")
-        data = {result[0].title: result[0].views}
+        data = {result[0].keyword: result[0].views}
         return Response(data)
     
 class PostRequest(APIView):
     def post(self, request):
-        data = {request.data['title']: 109}
-        return Response(data)
+        keyword = request.data['keyword']
+        start_time = request.data['startTime']
+        end_time = request.data['endTime']
+        months = self.get_months(start_time, end_time)
+        views = self.get_views(months, keyword, int(start_time.split('-')[-1]), int(end_time.split('-')[-1]))
+        data = {'keyword': keyword, 'views': views}
+        return Response(json.dumps(data))
+    
+    def get_months(self, start_time, end_time):
+        months = []
+        start_year, start_month, _ = start_time.split('-')
+        end_year, end_month, _ = end_time.split('-')
+        if (start_year == end_year):
+            for month in range(int(start_month), int(end_month) + 1):
+                months.append(f"{start_year}-{month:02d}")
+            return months
+        for year in range(int(start_year), int(end_year) + 1):
+            if year == int(start_year):
+                for month in range(int(start_month), 13):
+                    months.append(f"{year}-{month:02d}")
+            elif year == int(end_year):
+                for month in range(1, int(end_month) + 1):
+                    months.append(f"{year}-{month:02d}")
+            else:
+                for month in range(1, 13):
+                    months.append(f"{year}-{month:02d}")
+        return months
+    
+    def get_views(self, months, keyword, start_date, end_date):
+        views = {}
+        for month in months:
+            results = Wikipage.objects.filter(month=month, keyword=keyword)
+            if results.count() == 0:
+                return Response("No such record")
+            for result in results:
+                nums = [int(x) for x in result.views.split("-")]
+                if (len(months) == 1):
+                    for i in range(start_date - 1, end_date):
+                        views[f"{month}-{i + 1:02d}"] = nums[i]
+                    return views
+                if (month == months[0]):
+                    for i in range(start_date - 1, len(nums)):
+                        views[f"{month}-{i + 1:02d}"] = nums[i]
+                elif (month == months[-1]):
+                    for i in range(0, end_date):
+                        views[f"{month}-{i + 1:02d}"] = nums[i]
+                else:
+                    for i in range(0, len(nums)):
+                        views[f"{month}-{i + 1:02d}"] = nums[i]
+        return views
