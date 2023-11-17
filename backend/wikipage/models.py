98275f14c768f1fe6369fc2082b2052c99ce8003
Sa Wang <sawang@Sas-MacBook-Pro.home>
2023-11-16 22:31:29 -0800

    [backend] change column title to column pageid

--- backend/wikipage/models.py
+++ backend/wikipage/models.py
@@ -6,13 +6,13 @@ import datetime
 class Wikipage(models.Model):
     month = models.CharField(max_length=32, default='0000-00')
     keyword = models.CharField(max_length=32, default='')
-    title = models.CharField(max_length=128, default='')
+    pageid = models.CharField(max_length=128, default='')
     views = models.CharField(max_length=256)
 
     class Meta:
         indexes = [
-            models.Index(fields=['month', 'keyword', 'title'], name='idx_MKT')
+            models.Index(fields=['month', 'keyword', 'pageid'], name='idx_MKT')
         ]
 
     def __str__(self):
-        return self.title
+        return self.pageid
